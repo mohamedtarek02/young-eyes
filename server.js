142cd01dd011188249830710e217373eff86aa10
M TAREK <mohamedtarek074168@gmail.com>
2022-06-25 15:32:58 +0200

    test

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-const express = require("express ");
+const express = require("express");
 const serveStatic = require("serve-static");
 const path = require("path");
 
 const app = express();
 
-app.use("/news", serveStatic(path.join(__dirname, "/dist")));
+app.use("/", serveStatic(path.join(__dirname, "/dist")));
 
-const port = process.env.PORTy || 8080;
+const port = process.env.PORT || 8080;
 app.listen(port);
 
 console.log("listening on port: ", +port);
